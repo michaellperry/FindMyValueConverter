9f29139221943bbcd9787be1c294c1d964dfd7bc
Michael L Perry <MichaelPerry@GameStop.com>
2014-05-27 17:17:56 -0500

    Starting point.

---
+++ FindMyValueConverter.App/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FindMyValueConverter.App
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _on;
+
+        public bool On
+        {
+            get { return _on; }
+            set
+            {
+                _on = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("On"));
+            }
+        }
+    }
+}
